65c70fa3e06ea28c5d5ab764dc90511c95f57ce3
Vivek Burnwal <viv14092000@gmail.com>
2023-08-15 20:18:47 +0530

    read more added

--- app.js
+++ app.js
@@ -4,3 +4,20 @@ menu.onclick=() =>{
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 }
+
+let moreBtn= document.querySelectorAll('.more-btn');
+let p= document.querySelectorAll('.text');
+
+
+for(let i=0;i<moreBtn.length;i++){
+    moreBtn[i].addEventListener("click",(e)=>{
+        p[i].classList.toggle('show-more');
+        if(moreBtn[i].innerText==='Read More'){
+            moreBtn[i].innerText='Read Less';
+        }
+        else{
+            moreBtn[i].innerText='Read More'
+        }
+    })
+    
+}
